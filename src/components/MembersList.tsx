ee4226d0c4a423ec2c8c8901998120b00f026ac9
Denis Mory <denismr7@gmail.com>
2020-07-09 18:27:23 +0200

    Removed mockup data

--- src/components/MembersList.tsx
+++ src/components/MembersList.tsx
@@ -8,7 +8,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-import { mockData } from '../mockdata';
 import { Member } from '../interfaces';
 
 const useStyles = makeStyles({
