7bfd336803bfc7f72be27546a7322878ef14dc7d
Denis Mory <denismr7@gmail.com>
2020-07-09 09:05:19 +0200

    first commit

---
+++ src/components/MembersList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// URL: https://api.github.com/orgs/lemoncode/members
+
+export default function MembersList() {
+    return (
+        <div>
+            Hello from MembersList!
+        </div>
+    )
+}
