397bfc1c8d8aeeb35d27cffcd98cdaba2723a6b2
Denis Mory <denismr7@gmail.com>
2020-07-12 11:19:41 +0200

    Añadida paginación

--- src/components/MembersList.tsx
+++ src/components/MembersList.tsx
@@ -7,6 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TablePagination from '@material-ui/core/TablePagination';
 
 import { Member } from '../interfaces';
 
@@ -41,15 +42,26 @@ export default function MembersList(props: any) {
     const url: string = `https://api.github.com/orgs/${organization}/members`;
     const classes = useStyles();
     const [data, setData] = useState([])
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
 
     useEffect(() => {
         fetch(url).then(response => response.json())
                   .then(d => setData(d))
                   .catch(e => console.error(`Error in fetching data: ${e}`))
-    }, [url])
+    }, [url]);
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
     
-    const generateMemberRow = (MembersArray: Member[]): ReactNode => 
-        MembersArray.map(member => {
+    const generateMemberRow = (MembersArray: Member[]): ReactNode => {
+        return MembersArray.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(member => {
             return (
             <StyledTableRow key={member.id}>
                 <TableCell><img src={member.avatar_url} alt='member'/></TableCell>
@@ -58,9 +70,11 @@ export default function MembersList(props: any) {
             </StyledTableRow>
             )
         })
+    }
 
     return (
-        <TableContainer className={classes.table} component={Paper}>
+        <Paper className={classes.table}>
+        <TableContainer>
             <Table aria-label="members">
                 <TableHead className='TableHead'>
                     <TableRow>
@@ -73,6 +87,16 @@ export default function MembersList(props: any) {
                     {generateMemberRow(data)}
                 </TableBody>
             </Table>
+        <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="table"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
         </TableContainer>
+        </Paper>
     )
 }
